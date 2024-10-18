21ef3647ac0e11741b924158ce43e0c9aef3322a
Victoria <mularvika03@email.com>
2024-10-18 22:06:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myfragmentapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myfragmentapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myfragmentapp.R
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FirstFragment())
+                .commit()
+        }
+    }
+}
